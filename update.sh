6763d61d7650348d96e8a0227ed595d1f40a5006
Mixilver <156792479+Mixilver@users.noreply.github.com>
2024-12-19 16:01:37 +0500

    optimize to FoxetGSI

--- update.sh
+++ update.sh
@@ -10,5 +10,5 @@ if [[ -d "$TOOLS_DIR/Firmware_extractor" ]]; then
     git -C "$TOOLS_DIR"/Firmware_extractor fetch origin
     git -C "$TOOLS_DIR"/Firmware_extractor reset --hard origin/master
 else
-    git clone -q https://github.com/erfanoabdi/Firmware_extractor "$TOOLS_DIR"/Firmware_extractor
+    git clone -q https://github.com/FoxetGSI/Firmware_extractor.git "$TOOLS_DIR"/Firmware_extractor
 fi
