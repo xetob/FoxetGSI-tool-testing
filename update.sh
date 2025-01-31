7c24c5aed33f2f044f3d960bd922aa72e1f39a0e
xetob <xetob1122@gmail.com>
2025-01-31 12:13:18 +0300

    log if not install

--- update.sh
+++ update.sh
@@ -10,5 +10,6 @@ if [[ -d "$TOOLS_DIR/Firmware_extractor" ]]; then
     git -C "$TOOLS_DIR"/Firmware_extractor fetch origin
     git -C "$TOOLS_DIR"/Firmware_extractor reset --hard origin/master
 else
+    echo Installing Firmware_extractor 
     git clone -q https://github.com/FoxetGSI/Firmware_extractor.git "$TOOLS_DIR"/Firmware_extractor
 fi
