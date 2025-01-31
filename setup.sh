a2c068562bfa01dcbd59f6ae98d69174510f143f
xetob <xetob1122@gmail.com>
2025-02-01 00:47:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     id_like=$(awk -F= '$1 == "ID_LIKE" {print $2}' /etc/os-release)
     if [[ "$distro" == "arch" || "$id_like" == "arch" ]]; then
        echo "Arch Linux Detected"
-       sudo pacman -S --needed unace unrar zip unzip p7zip default-jre sharutils uudeview arj cabextract file-roller dtc xz python3-pip python-is-python3 brotli lz4 gawk libmpack aria2
+       sudo pacman -S unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python3 python-pip brotli lz4 gawk aria2 jre-openjdk -y
        #aur=rar
     else
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip python-is-python3 brotli liblz4-tool gawk aria2 default-jre
+       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip python-is-python3 brotli liblz4-tool gawk aria2 default-jre -y
     fi
     pip install --upgrade pip > /dev/null 2>&1 
     pip install --upgrade pip --break-system-packages > /dev/null 2>&1
