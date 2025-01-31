4b1e25208aaa4717255a795ad9121220760cd0c6
xetob <xetob1122@gmail.com>
2025-02-01 01:51:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 # Debian-based
        sudo apt update && sudo apt upgrade -y
        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip python-is-python3 brotli liblz4-tool gawk aria2 default-jre -y
-    python3 pip install --upgrade pip > /dev/null 2>&1 
-    python3 pip install --upgrade pip --break-system-packages > /dev/null 2>&1
-    python3 pip install backports.lzma pycrypto --break-system-packages
-    python3 pip install tqdm --break-system-packages
-    python3 pip install protobuf==5.27.3 six==1.16.0 bsdiff4==1.1.5 brotli==1.1.0 zstandard==0.23.0 --break-system-packages
+    pip3 install --upgrade pip > /dev/null 2>&1 
+    pip3 install --upgrade pip --break-system-packages > /dev/null 2>&1
+    pip3 install backports.lzma pycrypto --break-system-packages
+    pip3 install tqdm --break-system-packages
+    pip3 install protobuf==5.27.3 six==1.16.0 bsdiff4==1.1.5 brotli==1.1.0 zstandard==0.23.0 --break-system-packages
