ae1cfd639937eb72d3d62f671e99fdea3c002abb
xetob <xetob1122@gmail.com>
2025-02-01 16:14:45 +0300

    Update make.sh

--- prebuilt/12/make.sh
+++ prebuilt/12/make.sh
@@ -4,6 +4,11 @@ systempath=$1
 romdir=$2
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
+# Deal with non-flattened apex
+#$thispath/../../scripts/apex_extractor.sh $1/apex
+#$thispath/../../scripts/apex_extractor.sh $1/system_ext/apex
+echo "ro.apex.updatable=true" >> $1/product/etc/build.prop
+
 # Copy system files
 rsync -ra $thispath/system/ $systempath
 
@@ -42,19 +47,27 @@ sed -i "/typetransition location_app/d" $1/etc/selinux/plat_sepolicy.cil
 # Drop reboot_on_failure of init.rc
 sed -i "/reboot_on_failure/d" $1/etc/init/hw/init.rc
 
+# GSI always generate dex pre-opt in system image
+echo "ro.cp_system_other_odex=0" >> $1/product/etc/build.prop
+
+# GSI disables non-AOSP nnapi extensions on product partition
+echo "ro.nnapi.extensions.deny_on_product=true" >> $1/product/etc/build.prop
+
+# TODO(b/136212765): the default for LMK
+echo "ro.lmk.kill_heaviest_task=true" >> $1/product/etc/build.prop
+echo "ro.lmk.kill_timeout_ms=100" >> $1/product/etc/build.prop
+echo "ro.lmk.use_minfree_levels=true" >> $1/product/etc/build.prop
+
 #sudo sed -i "s|/dev/uinput               0660   uhid       uhid|/dev/uinput               0660   system     bluetooth|" $1/etc/ueventd.rc
 
 # Disable bpfloader
 rm -rf $1/etc/init/bpfloader.rc
-
-# product-prop patches
-echo "ro.apex.updatable=true" >> $1/product/etc/build.prop
-echo "ro.cp_system_other_odex=0" >> $1/product/etc/build.prop
-echo "ro.nnapi.extensions.deny_on_product=true" >> $1/product/etc/build.prop
-echo "ro.lmk.kill_heaviest_task=true" >> $1/product/etc/build.prop
-echo "ro.lmk.kill_timeout_ms=100" >> $1/product/etc/build.prop
-echo "ro.lmk.use_minfree_levels=true" >> $1/product/etc/build.prop
 echo "bpf.progs_loaded=1" >> $1/product/etc/build.prop
+
+# Don't write binary XML files
+echo "persist.sys.binary_xml=false" >> $1/build.prop
+
+# Bypass SF validateSysprops
 echo "ro.surface_flinger.vsync_event_phase_offset_ns=-1" >> $1/product/etc/build.prop
 echo "ro.surface_flinger.vsync_sf_event_phase_offset_ns=-1" >> $1/product/etc/build.prop
 echo "debug.sf.high_fps_late_app_phase_offset_ns=" >> $1/product/etc/build.prop
@@ -67,21 +80,3 @@ echo "debug.sf.high_fps_early_phase_offset_ns=" >> $1/product/etc/build.prop
 echo "debug.sf.high_fps_early_gl_phase_offset_ns=" >> $1/product/etc/build.prop
 echo "debug.sf.high_fps_early_app_phase_offset_ns=" >> $1/product/etc/build.prop
 echo "debug.sf.high_fps_early_gl_app_phase_offset_ns=" >> $1/product/etc/build.prop
-echo "ro.boot.vendor.overlay.theme=com.google.android.systemui.gxoverlay" >> $1/product/etc/build.prop
-echo "ro.config.ringtone=The_big_adventure.ogg" >> $1/product/etc/build.prop
-echo "ro.config.notification_sound=Popcorn.ogg" >> $1/product/etc/build.prop
-echo "ro.config.alarm_alert=Bright_morning.ogg" >> $1/product/etc/build.prop
-echo "persist.sys.overlay.pixelrecents=true" >> $1/product/etc/build.prop
-echo "qemu.hw.mainkeys=0" >> $1/product/etc/build.prop
-echo "ro.opa.eligible_device=true" >> $1/product/etc/build.prop
-echo "ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent" >> $1/product/etc/build.prop
-echo "ro.com.android.dataroaming=false" >> $1/product/etc/build.prop
-echo "ro.com.google.clientidbase=android-google" >> $1/product/etc/build.prop
-echo "ro.error.receiver.system.apps=com.google.android.gms" >> $1/product/etc/build.prop
-echo "ro.com.google.ime.theme_id=5" >> $1/product/etc/build.prop
-echo "ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms" >> $1/product/etc/build.prop
-echo "ro.com.google.ime.height_ratio=1.0" >> $1/product/etc/build.prop
-echo "ro.setupwizard.mode=OPTIONAL" >> $1/product/etc/build.prop
-echo "ro.vndk.lite=false" >> $1/product/etc/build.prop
-echo "persist.sys.disable_rescue=true" >> $1/product/etc/build.prop
-echo "ro.control_privapp_permissions=disable" >> $1/product/etc/build.prop
