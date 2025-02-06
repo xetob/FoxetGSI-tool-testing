951a84547e33816de759913adc6fb339153b59bc
xetob <xetob1122@gmail.com>
2025-02-06 21:44:03 +0300

    Update apex_extractor.sh

--- scripts/apex_extractor.sh
+++ scripts/apex_extractor.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
-APEXEXTRACT=$LOCALDIR/../tools/apex_tools/deapexer.py
+EXT4EXTRACT=$LOCALDIR/../tools/ext4extract/ext4extract.py
 
 TMPDIR=$LOCALDIR/../tmp/apex_ext
 mkdir -p "$TMPDIR"
 
 APEXDIR="$1"
-APEXES=$(ls "$APEXDIR" | grep ".*apex")
+APEXES=$(ls "$APEXDIR" | grep ".apex")
 for APEX in $APEXES; do
-    APEXNAME=$(echo "$APEX" | sed 's/\.[^.]*$//')
+    APEXNAME=$(echo "$APEX" | sed 's/.apex//')
     if [[ -d "$APEXDIR/$APEXNAME" ]]; then
         continue
     fi
-    if [[ $APEX == *".capex" ]]; then
-        7z e -y "$APEXDIR/$APEX" original_apex -o"$APEXDIR" 2>/dev/null >> "$TMPDIR"/zip.log
-        mv -f "$APEXDIR/original_apex" "$APEXDIR/$APEXNAME.apex"
-        rm -rf "$APEXDIR/$APEX"
-    fi
     mkdir -p "$APEXDIR/$APEXNAME"
-    7z e -y "$APEXDIR/$APEX" apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
-    python3 $APEXEXTRACT --debugfs_path "$(which debugfs)" --fsckerofs_path "$(which fsck.erofs)" extract "$APEXDIR/$APEXNAME.apex" "$APEXDIR/$APEXNAME"
-    rm "$APEXDIR/$APEXNAME.apex"
+    7z e "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
+    $EXT4EXTRACT "$APEXDIR/$APEXNAME/apex_payload.img" -D "$APEXDIR/$APEXNAME" 2>/dev/null
+    rm "$APEXDIR/$APEXNAME/apex_payload.img"
+    rm -rf "$APEXDIR/$APEXNAME/lost+found"
 done
 
 rm -rf "$TMPDIR"
