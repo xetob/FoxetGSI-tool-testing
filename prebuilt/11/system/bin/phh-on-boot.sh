56f927507d1ad840c3286e2007b560e2b28ce178
xetob <xetob1122@gmail.com>
2025-02-07 00:27:25 +0300

    im  wanna sleep

--- prebuilt/11/system/bin/phh-on-boot.sh
+++ prebuilt/11/system/bin/phh-on-boot.sh
@@ -1,6 +1,11 @@
 #!/system/bin/sh
 
+vndk="$(getprop persist.sys.vndk)"
+[ -z "$vndk" ] && vndk="$(getprop ro.vndk.version |grep -oE '^[0-9]+')"
+
 [ "$(getprop vold.decrypt)" = "trigger_restart_min_framework" ] && exit 0
+
+setprop ctl.start media.swcodec
 
 for i in wpa p2p;do
 	if [ ! -f /data/misc/wifi/${i}_supplicant.conf ];then
@@ -25,6 +30,26 @@ fi
 setprop ctl.restart sec-light-hal-2-0
 if find /sys/firmware -name support_fod |grep -qE .;then
 	setprop ctl.restart vendor.fps_hal
+fi
+
+setprop ctl.stop storageproxyd
+
+sleep 10
+
+minijailSrc=/system/system_ext/apex/com.android.vndk.v28/lib/libminijail.so
+minijailSrc64=/system/system_ext/apex/com.android.vndk.v28/lib64/libminijail.so
+if [ "$vndk" = 27 ];then
+    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
+    mount $minijailSrc /vendor/lib/libminijail_vendor.so
+fi
+
+if [ "$vndk" = 28 ];then
+    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
+    mount $minijailSrc /vendor/lib/libminijail_vendor.so
+    mount $minijailSrc64 /system/lib64/vndk-28/libminijail.so
+    mount $minijailSrc /system/lib/vndk-28/libminijail.so
+    mount $minijailSrc64 /vendor/lib64/libminijail.so
+    mount $minijailSrc /vendor/lib/libminijail.so
 fi
 
 #Clear looping services
