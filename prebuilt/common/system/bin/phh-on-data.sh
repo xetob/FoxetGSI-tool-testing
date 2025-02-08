ce63ca36940ca5977bddee23eeb931476ff656bb
xetob <xetob1122@gmail.com>
2025-02-08 11:12:39 +0300

    move phh.sh files to global

---
+++ prebuilt/common/system/bin/phh-on-data.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+
+vndk="$(getprop persist.sys.vndk)"
+[ -z "$vndk" ] && vndk="$(getprop ro.vndk.version |grep -oE '^[0-9]+')"
+
+if getprop persist.sys.phh.no_vendor_overlay |grep -q true;then
+	for part in odm vendor;do
+		mount /mnt/phh/empty_dir/ /$part/overlay
+	done
+fi
+
+if getprop persist.sys.phh.caf.media_profile |grep -q true;then
+    setprop media.settings.xml "/vendor/etc/media_profiles_vendor.xml"
+fi
