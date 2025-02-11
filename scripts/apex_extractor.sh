9f22d390e073af9ece2c93a5efd071e154127fcc
xetob <xetob1122@gmail.com>
2025-02-11 19:27:30 +0300

    idk

--- scripts/apex_extractor.sh
+++ scripts/apex_extractor.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+install_with_fallback() {
+    package_name=$1
+    pip3 install $package_name
+    if [ $? -ne 0 ]; then
+        echo "Error installing $package_name without --break-system-packages. Attempting to install with the flag."
+        pip3 install $package_name --break-system-packages
+    fi
+}
+
+install_with_fallback "protobuf==3.19.0"
+
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 APEXEXTRACT=$LOCALDIR/../tools/apex_tools/deapexer.py
 
@@ -23,5 +34,7 @@ for APEX in $APEXES; do
     python3 $APEXEXTRACT --debugfs_path "$(which debugfs)" --fsckerofs_path "$(which fsck.erofs)" extract "$APEXDIR/$APEXNAME.apex" "$APEXDIR/$APEXNAME"
     rm "$APEXDIR/$APEXNAME.apex"
 done
+
+install_with_fallback "protobuf==5.27.3"
 
 rm -rf "$TMPDIR"
