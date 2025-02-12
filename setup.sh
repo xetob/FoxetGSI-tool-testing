8e8b790eb57d9a41d0d02fa5069835ccea0fcfec
xetob <xetob1122@gmail.com>
2025-02-12 13:19:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,17 +10,7 @@ install_with_fallback() {
 }
 
 sudo apt update
-sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip python-is-python3 brotli liblz4-tool gawk aria2 default-jre erofs-utils -y
+sudo apt install openjdk-8-jdk nace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract rename liblzma-dev python3-pip brotli lz4 protobuf-compiler git gawk erofs-utils -y
 
 # Install Python packages
-install_with_fallback "backports.lzma"
-install_with_fallback "PyCryptodome"
-install_with_fallback "tqdm"
-install_with_fallback "testresources"
-install_with_fallback "protobuf==5.27.3"
-install_with_fallback "six==1.16.0"
-install_with_fallback "bsdiff4==1.1.5"
-install_with_fallback "brotli==1.1.0"
-install_with_fallback "zstandard==0.23.0"
-install_with_fallback "twrpdtgen"
-install_with_fallback "extract-dtb"
+install_with_fallback "backports.lzma protobuf pycrypto twrpdtgen extract-dtb pycryptodome"
