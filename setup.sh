56c1755b27641e0899a9cac7b14072c35c97b5a6
xetob <xetob1122@gmail.com>
2025-02-12 13:21:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,4 +13,4 @@ sudo apt update
 sudo apt install openjdk-8-jdk nace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract rename liblzma-dev python3-pip brotli lz4 protobuf-compiler git gawk erofs-utils -y
 
 # Install Python packages
-install_with_fallback "backports.lzma protobuf pycrypto twrpdtgen extract-dtb pycryptodome"
+install_with_fallback "backports.lzma protobuf twrpdtgen extract-dtb pycryptodome"
