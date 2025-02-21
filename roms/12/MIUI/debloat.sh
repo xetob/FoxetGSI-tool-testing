d5a03d713d733b36781b5afc57b6fa43682edb22
xetob <xetob1122@gmail.com>
2025-02-21 20:48:03 +0300

    MIUI12: Add some debloat for Global-based rom

--- roms/12/MIUI/debloat.sh
+++ roms/12/MIUI/debloat.sh
@@ -19,19 +19,28 @@ rm -rf $1/app/MiuiAudioMonitor
 rm -rf $1/app/MiuiBugReport
 rm -rf $1/app/MiuiDaemon
 rm -rf $1/app/MIUIDeskClock
-rm -rf $1/app/MIUIFileExplorer
+rm -rf $1/app/MIUIFileExplorer*
 rm -rf $1/app/MIUIFrequentPhrase
 rm -rf $1/app/MIUIgreenguard
 rm -rf $1/app/MiuiPrintSpoolerBeta
 rm -rf $1/app/MiuiSuperMarket
 rm -rf $1/app/MIUPTsmService
-rm -rf $1/app/MSA
+rm -rf $1/app/MSA*
 rm -rf $1/app/OtaProvision
 rm -rf $1/app/PaymentService
 rm -rf $1/app/SogouInput
 rm -rf $1/app/Updater
 rm -rf $1/app/VoiceAssist
 rm -rf $1/app/VoiceTrigger
+rm -rf $1/app/AnalyticsCore
+rm -rf $1/app/AutoTest
+rm -rf $1/app/facebook*
+rm -rf $1/app/MIRadioGlobalBuiltin
+rm -rf $1/app/MIUIDeskClockGlobalS
+rm -rf $1/app/MIUIMiPicks
+rm -rf $1/app/MIUIVideoPlayer
+rm -rf $1/app/MIUISystemAppUpdater
+rm -rf $1/app/MIUIHealthGlobal
 
 rm -rf $1/priv-app/MiGameCenterSDKService
 rm -rf $1/priv-app/MiBrowser
@@ -39,11 +48,11 @@ rm -rf $1/priv-app/MirrorSmartHub2
 rm -rf $1/priv-app/MIService
 rm -rf $1/priv-app/MIShare
 rm -rf $1/priv-app/MIUICalendar
-rm -rf $1/priv-app/MIUIMusic
+rm -rf $1/priv-app/MIUIMusic*
 rm -rf $1/priv-app/MIUIPersonalAssistant
 rm -rf $1/priv-app/MIUIQuickSearchBox
 rm -rf $1/priv-app/MIUIVideo
-rm -rf $1/priv-app/MIUIYellowPage
+rm -rf $1/priv-app/MIUIYellowPage*
 
 rm -rf $1/data-app/*
 rm -rf $1/product/data-app/*
@@ -51,3 +60,9 @@ rm -rf $1/product/data-app/*
 rm -rf $1/product/app/aiasst_service
 rm -rf $1/product/app/talkback
 rm -rf $1/product/priv-app/ConfigUpdater
+rm -rf $1/product/app/CalendarGoogle
+rm -rf $1/product/app/Chrome
+rm -rf $1/product/app/Gmail2
+rm -rf $1/product/app/GoogleOne
+rm -rf $1/product/app/Maps
+rm -rf $1/product/app/YouTube
