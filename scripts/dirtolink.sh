1183636aa8ca3c86978e0a9ae54d4eb219140d9c
xetob <xetob1122@gmail.com>
2025-02-22 23:50:39 +0300

    dirtolink: Added move root/dir to root/system/dir if its not empty

--- scripts/dirtolink.sh
+++ scripts/dirtolink.sh
@@ -4,12 +4,24 @@ systempath=$1
 romdir=$2
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-if [ -e "$1/../product" ]; then
+is_directory_empty() {
+    [ -z "$(ls -A "$1")" ]
+}
+
+if [ -e "$1/../product" ] || [ -L "$1/../product" ]; then
+    if [ -d "$1/../product" ] && ! is_directory_empty "$1/../product"; then
+        mv "$1/../product"/* "$1"/ 2>/dev/null
+        mv "$1/../product"/.[!.]* "$1"/ 2>/dev/null
+    fi
     rm -rf "$1/../product"
     ln -s /system/product "$1/../product"
 fi
 
-if [ -e "$1/../system_ext" ]; then
+if [ -e "$1/../system_ext" ] || [ -L "$1/../system_ext" ]; then
+    if [ -d "$1/../system_ext" ] && ! is_directory_empty "$1/../system_ext"; then
+        mv "$1/../system_ext"/* "$1"/ 2>/dev/null
+        mv "$1/../system_ext"/.[!.]* "$1"/ 2>/dev/null
+    fi
     rm -rf "$1/../system_ext"
     ln -s /system/system_ext "$1/../system_ext"
 fi
